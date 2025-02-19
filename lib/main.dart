47450f2ee3fb07c3e5b5b04b23eafd41d9fd4987
Lexus04-Lex <7526878@gmail.com>
2025-02-19 14:28:37 +0300

    Flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,75 @@
 import 'package:flutter/material.dart';
 import 'package:weconnect/pages/home.dart';
+import 'package:weconnect/pages/settings.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main(){
+void main()async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://xkufmazhwkemukcnsgsr.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhrdWZtYXpod2tlbXVrY25zZ3NyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgzMjI2MTYsImV4cCI6MjA1Mzg5ODYxNn0.KFfpRmIyRfHALLFD49sFurizzSLkdPNEeHyfiEGlA7k',
+
+  );
+  
+
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      
       theme: ThemeData(),
-      home: const Homepage(),
+      home: const Homepage(
+        
+      ),
       debugShowCheckedModeBanner: false,
       debugShowMaterialGrid: false,
     );
   }
-
   }
-
   class Homepage extends StatelessWidget {
   const Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // final  title= '';
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.blueGrey,
-        centerTitle: true,
-        elevation: 10,
-        shadowColor: Colors.cyanAccent,
-        title: Text('My App'),
-      ),
-      body: const BodyWidget(),
+      
+
+      // appBar: MyAppBar(title: title),
+    
+      body: const BodyWidget()
+    );
+  }
+}
+
+class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
+  final String title;
+  final double headerheight = 70.0;
+  const MyAppBar({super.key, required this.title});
+
+  @override
+  Size get preferredSize => Size.fromHeight(headerheight);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      elevation: 10,
+      backgroundColor: Colors.grey,
+      title: Text(title),
+      centerTitle: true,
+      actions: [
+        IconButton(
+          onPressed: ()=> Navigator.push(context,
+           MaterialPageRoute(builder: 
+           (context)=> const Settings())), 
+           icon: Icon(Icons.settings),
+           
+           )
+      ],
+      
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:weconnect/main.dart';
+import 'package:weconnect/pages/homepage.dart';
+import 'package:weconnect/pages/menu.dart';
+import 'package:weconnect/pages/profile.dart';
+import 'package:weconnect/pages/settings.dart';
 
 class BodyWidget extends StatefulWidget {
   const BodyWidget({super.key});
@@ -8,25 +13,103 @@ class BodyWidget extends StatefulWidget {
 }
 
 class _BodyWidgetState extends State<BodyWidget> {
+    int _selectedIndex = 0;
+
+    late final List<Widget> _pages;
+
+    @override
+    void initState(){
+      super.initState();
+      _pages = [
+        //Home page
+        Home(),
+
+        //Menu page
+
+        Menu(),
+
+        //About page
+        Profile(),
+
+        //Settings page
+        Settings(),
+
+      ];
+    }
+
+    // @override
+    void _onItemtapped(int index){
+      setState(() {
+        _selectedIndex = index;
+      });
+    }
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.greenAccent,
-      padding: EdgeInsets.all(20),
-      margin: EdgeInsets.all(20),
+    final String title = "Lexus";
+    return Scaffold(
+      appBar: MyAppBar(title: title),
+               drawer: Drawer(
+          backgroundColor: const Color.fromARGB(255, 77, 129, 219),
+          elevation: 10.0,
+          child: ListView(
+            padding: EdgeInsets.only(left: 25, right: 25),
+            children: [
+              UserAccountsDrawerHeader(
+                
+                accountName: Text('Isabirye Alex'),
+                accountEmail: Text('isabiryealexx@gmail.com'),
+                currentAccountPicture: CircleAvatar(
+                  backgroundImage: AssetImage('lib/assets/images/c5.jpeg'),
+                ), 
+                decoration: BoxDecoration(color: const Color.fromARGB(255, 15, 32, 32)),
+              ),
 
-      
-      
-      child: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Hello')
-          ],
-        ),
-      
-      ),
-      
+              
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Profile'),
+
+              ),
+
+              ListTile(
+                leading: Icon(Icons.person),
+                title: Text('Profile'),
+
+              ),
+            ],
+            
+          ),
+          
+         ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.blue,
+        selectedFontSize: 20,
+        selectedItemColor: Colors.blueGrey,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+        
+        items: const <BottomNavigationBarItem> [
+          BottomNavigationBarItem(icon: Icon(Icons.home),
+          label: 'Home'
+          
+          ),
+
+          BottomNavigationBarItem(icon: Icon(Icons.menu),
+          label: 'Shop'
+          ),
+
+          BottomNavigationBarItem(icon: Icon(Icons.person),
+          label: 'Profile'
+          ),
+
+          // BottomNavigationBarItem(icon: Icon(Icons.person),
+          // label: 'Profile'),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemtapped,
+        
+         ),
+
     );
   }
 }
